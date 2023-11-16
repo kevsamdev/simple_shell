67ee56d934147b9dfbb52e74f5dda62d67536024
kevsamdev <kevinsamson45@gmail.com>
2023-11-15 16:17:53 -0800

    simpleshell1

--- exit_shell.c
+++ exit_shell.c
@@ -7,8 +7,8 @@
  */
 void exit_shell(void)
 {
-	printf("Exiting the shell...\n");
-	exit(EXIT_SUCCESS);
+        printf("Exiting the shell...\n");
+        exit(EXIT_SUCCESS);
 }
 
 /**
@@ -18,26 +18,26 @@ void exit_shell(void)
  */
 int main(void)
 {
-	char input[100];
+        char input[100];
+        size_t len;
 
-	while (1)
-	{
-		printf("Shell> ");
-		fgets(input, sizeof(input), stdin);
-			size_t len = strlen(input);
-		if (len > 0 && input[len - 1] == '\n')
-		{
-			input[len - 1] = '\0';
-		}
+        while (1)
+        {
+                printf("Shell> ");
+                fgets(input, sizeof(input), stdin);
+                len = strlen(input);
+                if (len > 0 && input[len - 1] == '\n')
+                {
+                        input[len - 1] = '\0';
+                }
 
-		if (strcmp(input, "exit") == 0)
-		{
-			exit_shell();
-		}
+                if (strcmp(input, "exit") == 0)
+                {
+                        exit_shell();
+                }
 
-		printf("Command: %s\n", input);
-	}
+                printf("Command: %s\n", input);
+        }
 
-	return (0);
+        return (0);
 }
-
